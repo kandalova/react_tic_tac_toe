ef6733ee21b9eae075317e2c71dfa006601facf7
Leyla Kandalova <leyla_kandalova@epam.com>
2022-10-06 12:00:06 +0400

    Change Square component to function

--- src/index.js
+++ src/index.js
@@ -2,17 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={()=>{this.props.onClick()}}
-        >
-        {this.props.value}
-      </button>
-    );
-  }
+//class component
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button 
+//         className="square" 
+//         onClick={()=>{this.props.onClick()}}
+//         >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+//function component
+function Square(props){
+  return(
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
